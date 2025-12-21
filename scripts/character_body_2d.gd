a6fe8f299fd9f45fddc7dcdd885ef66f07947ff5
JoaquinOre <zombiesspace12@gmail.com>
2025-12-20 22:14:05 -0300

    actualizacion final para domingo
    fisicas de salto

--- scripts/character_body_2d.gd
+++ scripts/character_body_2d.gd
@@ -1,27 +1,46 @@
 extends CharacterBody2D
-var velocidad = 150
-@onready var player =$".."
-@onready var animacion =  $AnimatedSprite2D
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
+@export var jump_height : float 
+@export var jump_time_to_peak : float
+@export var jump_time_to_descent : float 
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(_delta: float) -> void:
-	if(Input.is_action_pressed("tecla_W")):
-		player.position.y -= velocidad * _delta
-		animacion.play("walk_up")
-	elif (Input.is_action_pressed("tecla_S")):
-		player.position.y += velocidad * _delta
-		animacion.play("walk_down")
-	elif(Input.is_action_pressed("tecla_A")):
-		player.position.x -= velocidad * _delta
-		animacion.play("walk_left")
-	elif(Input.is_action_pressed("tecla_D")):
-		player.position.x += velocidad * _delta
-		animacion.play("walk_right")
-	else: 
-		animacion.stop()
+
+const SPEED = 300.0
+@onready var JUMP_VELOCITY : float = ((2.0 * jump_height) / jump_time_to_peak) * - 1.0
+@onready var jump_gravity : float  = ((2.0 * jump_height) / (jump_time_to_peak * jump_time_to_peak)) * - 1.0
+@onready var fall_gravity : float = ((2.0 * jump_height) / (jump_time_to_descent * jump_time_to_descent)) * - 1.0
+@onready var animacion = $AnimatedSprite2D
+
+func _physics_process(delta: float) -> void:
+	# Add the gravity.
+	
+	velocity.y -= get_custom_gravity() * delta
+
+	# Handle jump.
+	if Input.is_action_just_pressed("ui_Space") and is_on_floor():
+		jump()
+
+	# Get the input direction and handle the movement/deceleration.
+	# As good practice, you should replace UI actions with custom gameplay actions.
+	var direction := Input.get_axis("tecla_A", "tecla_D")
+	if direction:
+		if direction > 0 :
+			animacion.play("walk_right")
+			animacion.flip_h = (direction < 0)
+		if direction < 0 :
+			animacion.play("walk_right")
+			animacion.flip_h  = (direction < 0)
+		velocity.x = direction * SPEED
+		
+	else:
+		velocity.x = move_toward(velocity.x, 0, SPEED)
+		animacion.play("idle")
+
 	move_and_slide()
+
+func get_custom_gravity() -> float:
+	return jump_gravity if velocity.y < 0.0 else fall_gravity
+
+func jump(): 
+	velocity.y = JUMP_VELOCITY
